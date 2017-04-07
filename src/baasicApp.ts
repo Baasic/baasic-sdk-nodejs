49d8558456b3864c145353c8685a146b98e0b2c4
Kristijan Horvat <kristijan@mono.hr>
2017-04-07 14:45:17 +0200

    Updated baasic app api

--- src/baasicApp.ts
+++ src/baasicApp.ts
@@ -1,35 +1,74 @@
-import { IBaasicOptions, BaasicApp as BaasicSdkApp } from 'baasic-sdk-javascript';
+import { IBaasicOptions, BaasicApp as BaasicSdkApp, IToken, IUser } from 'baasic-sdk-javascript';
 import { HttpClient } from './infrastructure/httpApi/http-client';
 import { InMemoryStorageHandler } from './infrastructure/store';
 import { EventHandler } from './infrastructure/event.handler';
-
-import {
-    ApplicationSettingsClient,
-    ArticleClient,
-    CommerceClient,
-    DynamicResourceClient,
-    FilesClient,
-    KeyValueClient,
-    MediaVaultClient,
-    MembershipClient,
-    MeteringClient,
-    NotificationsClient,
-    TemplatingClient,
-    UserProfileClient,
-    ValueSetClient
-} from './clients';
+import * as clients from './clients';
 
 
 export class BaasicApp {
-    private readonly app : BaasicSdkApp;
+    private readonly app: BaasicSdkApp;
+
+    public readonly membershipClient: clients.MembershipClient;
+    //Modules
+    public readonly applicationSettingClient: clients.ApplicationSettingsClient;
+    public readonly keyValueClient: clients.KeyValueClient;
+    public readonly valueSetClient: clients.ValueSetClient;
+    public readonly userProfileClient: clients.UserProfileClient;
+    public readonly templatingClient: clients.TemplatingClient;
+    public readonly meteringClient: clients.MeteringClient;
+    public readonly mediaVaultClient: clients.MediaVaultClient;
+    public readonly fileClient: clients.FilesClient;
+    public readonly dynamicResourceClient: clients.DynamicResourceClient;
+    public readonly notificationClient: clients.NotificationsClient;
+    public readonly articleClient: clients.ArticleClient;
+    public readonly commerceClient: clients.CommerceClient;
 
     constructor(apiKey: string, options?: Partial<IBaasicOptions>) {
         this.app = new BaasicSdkApp(apiKey, getOptions(options));
+
+        this.membershipClient = new clients.MembershipClient(this.app);
+        //Modules
+        this.applicationSettingClient = new clients.ApplicationSettingsClient(this.app);
+        this.keyValueClient = new clients.KeyValueClient(this.app);
+        this.valueSetClient = new clients.ValueSetClient(this.app);
+        this.userProfileClient = new clients.UserProfileClient(this.app);
+        this.templatingClient = new clients.TemplatingClient(this.app);
+        this.meteringClient = new clients.MeteringClient(this.app);
+        this.mediaVaultClient = new clients.MediaVaultClient(this.app);
+        this.fileClient = new clients.FilesClient(this.app);
+        this.dynamicResourceClient = new clients.DynamicResourceClient(this.app);
+        this.notificationClient = new clients.NotificationsClient(this.app);
+        this.articleClient = new clients.ArticleClient(this.app);
+        this.commerceClient = new clients.CommerceClient(this.app);
+    }
+
+    getAccessToken(): IToken {
+        return this.app.getAccessToken();
+    }
+
+    updateAccessToken(value: IToken) {
+        this.app.updateAccessToken(value);
+    }
+
+    getApiKey(): string {
+        return this.app.getApiKey();
+    }
+
+    getApiUrl(): URL {
+        return this.app.getApiUrl();
+    }
+
+    getUser(): IUser {
+        return this.app.getUser();
+    }
+
+    setUser(userInfo: IUser) {
+        this.app.setUser(userInfo);
     }
 };
 
 function getOptions(options: Partial<IBaasicOptions>): Partial<IBaasicOptions> {
-    var defaults : Partial<IBaasicOptions> = {
+    var defaults: Partial<IBaasicOptions> = {
         httpClient: () => new HttpClient(),
         storageHandler: () => new InMemoryStorageHandler(),
         eventHandler: () => new EventHandler()
